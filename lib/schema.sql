58f05ec70dd980d14e63295c41593d4938adabfd
Jawann <jawann@gmail.com>
2018-02-16 05:19:46 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE subgenre (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE series (id INTEGER PRIMARY KEY, title, TEXT, author_id INTEGER, subgenre_id INTEGER);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
